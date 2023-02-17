f228fd6e7da26c7f6eefa8fab8f3aa560b831b30
Alejandro Daniele <alejandro.daniele88@gmail.com>
2023-02-17 14:53:48 -0300

    PreEntrega1-Daniele

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/navbar/Navbar'
-import { Badge, Button } from '@mui/material';
+import { Badge } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
@@ -11,13 +11,12 @@ function App() {
   return (
     <div>
       <header>
-        <div className="header__div">
-          <img src="" alt="" />
           <h1 className="titulo">La web del chocolate</h1>
-          <Badge badgeContent={1} color="primary">
-            <ShoppingCartIcon color="white" />
-          </Badge>
-        </div>
+          <div className="cartWidget">
+            <Badge badgeContent={1} color="primary">
+              <ShoppingCartIcon color="white" />
+            </Badge>
+          </div>  
         <Navbar />
       </header>
       <main>
